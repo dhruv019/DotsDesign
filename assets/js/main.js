d63b1a1ea3953d9f50023cb51d76ebfd8030eb6b
Dhruv Tarachandani <dhruv.tarachandani@peregrineintegration.com>
2025-01-09 20:40:02 +0530

    updated projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -235,12 +235,12 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
-document.addEventListener('DOMContentLoaded', () => {
-  fetch('../helpers/header.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('header').innerHTML = data);
+// document.addEventListener('DOMContentLoaded', () => {
+//   fetch('../helpers/header.html')
+//     .then(response => response.text())
+//     .then(data => document.getElementById('header').innerHTML = data);
 
-  fetch('../helpers/footer.html')
-    .then(response => response.text())
-    .then(data => document.getElementById('footer').innerHTML = data);
-});
+//   fetch('../helpers/footer.html')
+//     .then(response => response.text())
+//     .then(data => document.getElementById('footer').innerHTML = data);
+// });
